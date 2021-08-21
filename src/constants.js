746afd8ff6042ed39063252e3357b62c5083544c
unknown <sayandeepbasu8199@gmail.com>
2021-08-21 10:15:47 +0530

    redeployment

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
 const PRODUCTION_URL = "http://immense-falls-27647.herokuapp.com";
 
-exports.API_URL = DEVELOPMENT_URL;
+exports.API_URL = PRODUCTION_URL;
